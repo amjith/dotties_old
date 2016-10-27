acb5294bf8ae1c5ba9f3152e53f7f20f59550425
Amjith Ramanujam <amjith.r@gmail.com>
2016-10-27 07:20:17 -0700

    Add pbcopy/pbpaste for linux.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,21 +1,22 @@
-set -g -x PATH $HOME/bin $HOME/anaconda/bin /usr/local/bin $PATH /usr/X11/bin 
+set -g -x PATH $HOME/bin $PATH
 set -g -x TERM screen-256color
 
 # Autojump settings
 if test -e /usr/local/share/autojump/autojump.fish 
     . /usr/local/share/autojump/autojump.fish
+else if test -e /usr/share/autojump/autojump.fish
+    . /usr/share/autojump/autojump.fish
 end
 
 # Virtualenv wrapper 
 set -g VIRTUALFISH_COMPAT_ALIASES
 . $HOME/.virtualfish/virtual.fish
 
-# Alias
+# Aliases
 alias l='ls -lah'
 
-# Sauce lab creds
-set -g -x JIL_SAUCE_LABS_USERNAME bam_dev
-set -g -x JIL_SAUCE_LABS_ACCESS_KEY 291fc8ed-7260-4d82-bc4c-fb2cb139f673
-
-
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+switch (uname)
+    case Linux
+        alias pbcopy 'xsel --clipboard --input'
+        alias pbpaste 'xsel --clipboard --output'
+end
